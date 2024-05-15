e210b2895a4cd3941a1d63d064ad32bcc0684dcc
Artem Gilmanov <84340745+artemgilmanov@users.noreply.github.com>
2024-05-15 09:23:27 +0200

    Add files via upload

---
+++ features/annotations/arrays.ts
@@ -0,0 +1,25 @@
+const carMakers: string[] = ['ford', 'toyota', 'chevy']
+
+const dates = [new Date(), new Date()]
+
+// const carsByMake: [][] = []
+const carsByMake = [['f150'], ['corolla'], ['camaro']]
+
+// Help with inference when extracting values
+const car = carMakers[0]
+const myCar = carMakers.pop()
+
+// Preven uncompatible values
+carMakers.push(100)
+
+// Help with "map"
+carMakers.map((car: string): string => {
+  return car.toUpperCase()
+})
+
+// Flexible types
+//const importantDates: (Date | string)[] = [new Date(), '2030-10-10']
+const importantDates = [new Date(), '2030-10-10']
+importantDates.push('2030-10-10')
+importantDates.push(new Date())
+importantDates.push(100)
